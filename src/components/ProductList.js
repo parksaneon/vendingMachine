da82fee9a78e61b8be3f15d2497d88007b22217b
parksaneon <sangeon1500@daum.net>
2022-02-28 13:08:37 +0900

    feat : ProductList component

---
+++ src/components/ProductList.js
@@ -0,0 +1,29 @@
+export default function ProductList({ $target, initialState }) {
+  const $productList = document.createElement('ul');
+  $target.appendChild($productList);
+
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    if (!this.state) return;
+
+    $productList.innerHTML = `${this.state
+      .map(
+        ({ imageUrl, name, price }) => `<li class="Product">
+        <img src="${imageUrl}"/>
+        <div class="Product__info">
+          <div>${name}</div>
+          <div>${price}</div>
+        </div>
+      </li>`
+      )
+      .join('')}`;
+  };
+
+  this.render();
+}
--- src/page/ProductListPage.js
+++ src/page/ProductListPage.js
@@ -1,10 +1,23 @@
+import request from '../util/request';
+
 export default function ProductListPage({ $target }) {
   const $page = document.createElement('div');
   $page.className = 'ProductListPage';
 
   $page.innerHTML = '<h1>상품 목록</h1>';
 
+  this.setState = (nextState) => {
+    this.state = nextState;
+  };
+
+  const fetchProducts = async () => {
+    const products = await request('/products');
+    this.setState(products);
+  };
+
   this.render = () => {
     $target.appendChild($page);
   };
+
+  fetchProducts();
 }
