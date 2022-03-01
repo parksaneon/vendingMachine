68388c89d832d48c5bda6fe98d9fba898bab3d88
ParkSangEon <sangeon1500@daum.net>
2022-03-01 20:04:55 +0900

    feat : start CartPage Component

--- src/page/CartPage.js
+++ src/page/CartPage.js
@@ -1,10 +1,21 @@
+import { getItem } from '../util/storage';
+
 export default function CartPage({ $target }) {
   const $page = document.createElement('div');
   $page.className = 'CartPage';
 
   $page.innerHTML = '<h1>장바구니</h1>';
 
+  this.state = { products: null };
+
+  const cartData = getItem('products_cart', []);
+
   this.render = () => {
-    $target.appendChild($page);
+    if (cartData.length === 0) {
+      alert('장바구니가 비어있습니다.');
+      routeChange('/');
+    } else {
+      $target.appendChild($page);
+    }
   };
 }
